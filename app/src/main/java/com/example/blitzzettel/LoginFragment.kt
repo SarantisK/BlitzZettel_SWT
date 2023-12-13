bbcc94648dd8a2a9bf4c6f5c3be2fff0dfa542ea
hyildirim <hyildirim@stud.hs-heilbronn.de>
2023-12-13 19:00:07 +0100

    Bug fix

---
+++ app/src/main/java/com/example/blitzzettel/LoginFragment.kt
@@ -0,0 +1,42 @@
+package com.example.blitzzettel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.blitzzettel.databinding.FragmentLoginBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.anmeldeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_FirstFragment)
+        }
+    }
+}
--- app/src/main/java/com/example/blitzzettel/MainActivity.kt
+++ app/src/main/java/com/example/blitzzettel/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.blitzzettel
 
-import android.R
-import android.app.Activity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
 import com.example.blitzzettel.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,10 +28,10 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        ///binding.addButton.setOnClickListener { view ->
-        ///    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        ///        .setAction("Action", null).show()
-        ///}
+        //binding.addButton.setOnClickListener { view ->
+        //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //    .setAction("Action", null).show()
+        // }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -59,3 +56,4 @@ class MainActivity : AppCompatActivity() {
                 || super.onSupportNavigateUp()
     }
 }
+
