c449baf61e899b190de1209b893f4f36307e1d8c
amaxharraj <amaxharraj@stud.hs-heilbronn.de>
2023-12-21 16:10:02 +0100

    UI

---
+++ app/src/main/java/com/example/blitzzettel/BlitzSettings.kt
@@ -0,0 +1,4 @@
+package com.example.blitzzettel
+
+class BlitzSettings {
+}
--- app/src/main/java/com/example/blitzzettel/MainActivity.kt
+++ app/src/main/java/com/example/blitzzettel/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -34,20 +35,14 @@ class MainActivity : AppCompatActivity() {
         // }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.settings -> findNavController(R.id.settings)
         }
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
