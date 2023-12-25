a65fbd70045beb6da019fc362a5d4565b30881d8
kusingh <kusingh@stud.hs-heilbronn.de>
2023-12-25 10:34:08 +0100

    autoLogin nach ersten Anmeldung

--- app/src/main/java/com/example/blitzzettel/LoginFragment.kt
+++ app/src/main/java/com/example/blitzzettel/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.blitzzettel
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -17,127 +18,96 @@ import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
+ * Use the [LoginFragment] factory method to
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
-    //private var token:String? = ""
 
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var encryptedPrefsManager: EncryptedPrefsManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        encryptedPrefsManager = EncryptedPrefsManager(requireContext())
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        //sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
-        val saveCred= EncryptedPrefsManager(requireContext())
-        binding.anmeldeButton.setOnClickListener{
+        if (isFirstTime()) {
+            updateFirstTimeStatus()
+            setupLoginListener()
+        } else {
+            autoLogin()
+        }
+    }
 
-            // Hole alle Daten aus den Textfeldern
-
+    private fun setupLoginListener() {
+        binding.anmeldeButton.setOnClickListener {
             val nutzername = binding.editTexNutzername.text.toString()
             val passwort = binding.editTextPasswort.text.toString()
             val serverId = binding.editTextServerid.text.toString()
-            val api = Api("",serverId)
 
-            if (nutzername.isBlank() || passwort.isBlank() || serverId.isBlank()) {
-                Toast.makeText(requireContext(), "Alle Felder ausfüllen", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+            if (validateInputs(nutzername, passwort, serverId)) {
+                performLogin(nutzername, passwort, serverId)
             }
-
-            lifecycleScope.launch {
-                // API-Aufruf zur Authentifizierung
-                val token = withContext(Dispatchers.IO) {
-                    api.postGenerateToken(nutzername, passwort)
-                }
-
-                if (token != "400") {
-                    val sharedViewModel: SharedViewModel by activityViewModels()
-                    sharedViewModel.ServerIP = serverId
-                    sharedViewModel.BearerToken = token // Class Viewmodel den Token übergeben
-                    saveCred.saveCredentials(nutzername, passwort, serverId)
-
-
-                    findNavController().navigate(R.id.action_loginFragment_to_HomeFragment)
-                } else {
-                    //Funktioniert noch nicht
-                    Toast.makeText(requireContext(), "Fehlgeschlagen", Toast.LENGTH_SHORT).show()
-                }
-            }
-
         }
+    }
+
+    private fun autoLogin() {
+        val (nutzername, passwort, serverId) = encryptedPrefsManager.getCredentials()
+        performLogin(nutzername ?: "", passwort ?: "", serverId ?: "")
+    }
 
 
+    private fun performLogin(nutzername: String, passwort: String, serverId: String) {
+        val api = Api("", serverId)
+        lifecycleScope.launch {
+            val token = withContext(Dispatchers.IO) {
+                api.postGenerateToken(nutzername, passwort)
+            }
 
-
-        /*if (isFirstTime()) {
-            updateFirstTimeStatus()
-            binding.anmeldeButton.setOnClickListener {
-                val nutzername = binding.editTextTextNutzername.text.toString()
-                val passwort = binding.editTextTextPassword.text.toString()
-                val serverId = binding.editTextServerid.text.toString()
-                // API Abfrage (PostGenerateToken)
-                cred.saveCredentials(nutzername, passwort, serverId)
-
-
-
-
-
+            if (token != "400" && token != "403") {
                 findNavController().navigate(R.id.action_loginFragment_to_HomeFragment)
+                val sharedViewModel: SharedViewModel by activityViewModels()
+                sharedViewModel.ServerIP = serverId
+                sharedViewModel.BearerToken = token
+                encryptedPrefsManager.saveCredentials(nutzername, passwort, serverId)
+                Toast.makeText(requireContext(), "Erfolgreich Angemeldet", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(requireContext(), "Fehlgeschlagen", Toast.LENGTH_SHORT).show()
             }
-            // Nach Aktionen beim ersten Start, navigiere zu einem anderen Fragment
-
-        } else {
-            cred.clearCredentials()
-            navigateToHomeIfCredentialsExist()
-        }*/
-
-
-/*
-        if (EncryptedPrefsManager.hasCredentials(requireContext())) {
-            val credentials = cred.getCredentials()
-            binding.editTextTextNutzername.setText(credentials.first)
-            binding.editTextTextPassword.setText(credentials.second)
-            binding.editTextServerid.setText(credentials.third)
-
-        }
-
-        binding.anmeldeButton.setOnClickListener {
-            val nutzername = binding.editTextTextNutzername.text.toString()
-            val passwort = binding.editTextTextPassword.text.toString()
-            val serverId = binding.editTextServerid.text.toString()
-            cred.saveCredentials(nutzername, passwort, serverId)
-
-            findNavController().navigate(R.id.action_loginFragment_to_HomeFragment)
-        }*/
-
-    }
-
-
-    private fun navigateToHomeIfCredentialsExist() {
-
-
-        if (EncryptedPrefsManager.hasCredentials(requireContext())) {
-            // Navigiere zum HomeFragment, wenn Anmeldeinformationen vorhanden sind
-            findNavController().navigate(R.id.action_loginFragment_to_HomeFragment)
         }
     }
+
+
+    private fun validateInputs(nutzername: String, passwort: String, serverId: String): Boolean {
+        if (nutzername.isBlank() || passwort.isBlank() || serverId.isBlank()) {
+            Toast.makeText(requireContext(), "Alle Felder ausfüllen", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
     private fun isFirstTime(): Boolean {
         return sharedPreferences.getBoolean("firstTime", true)
     }
 
     private fun updateFirstTimeStatus() {
-        // Setze den Zustand auf false, da die App jetzt gestartet wurde
         sharedPreferences.edit().putBoolean("firstTime", false).apply()
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
+
