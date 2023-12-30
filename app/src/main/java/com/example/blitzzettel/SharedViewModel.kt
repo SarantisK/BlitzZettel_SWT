a66654b2a412d99d7334ee6a7880b0582833e68b
hlambert <hlambert@stud.hs-heilbronn.de>
2023-12-30 17:14:50 +0100

    Icon, Kommentar, fragment_Home button

--- app/src/main/java/com/example/blitzzettel/SharedViewModel.kt
+++ app/src/main/java/com/example/blitzzettel/SharedViewModel.kt
@@ -2,6 +2,8 @@ package com.example.blitzzettel
 
 import androidx.lifecycle.ViewModel
 
+//Bearer-Token und IP werden in der Klasse gespeichert und vererbt
+
 class SharedViewModel:ViewModel() {
 
     var BearerToken : String? = null
